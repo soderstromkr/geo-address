79551e4865cdc1ceab4fb8cf60744c49246451c8
Kristofer Söderström <kristofer.soderstrom@gmail.com>
2023-02-13 21:38:31 +0100

    changed folder name and setup

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
-import setuptools
+from setuptools import setup
 
-descr = '''
-Dissagregates author addresses from Web of Science data.
-Calculates author to address multiplier.
-'''
-if __name__ == "__main__":
-	setuptools.setup(
-		name='geo_address', 
-		version="0.1",
-		author= 'Kristofer R. Söderström',
-		description='Companion package for article "ARTICLE_NAME" published \
-		in "JOURNAL NAME", LICENSE_TYPE, CITATION', #ADD MORE INFO HERE,
-		install_requires=['re', 'pandas', 'numpy', 'tqdm', 'geopy'],
-		#packages=["geo-address"]
+setup(
+	name='geo-address', 
+	version="0.1",
+	author= 'Kristofer R. Söderström',
+	description='Companion package for article "ARTICLE_NAME" published \
+	in "JOURNAL NAME", LICENSE_TYPE, CITATION', #ADD MORE INFO HERE,
+	install_requires=['re', 'pandas', 'numpy', 'tqdm', 'geopy'],
+	packages=["geo-address"]
  	)
