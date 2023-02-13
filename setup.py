729ccb758ce2d1cec0466f94dec712f514c59dcb
Kristofer Söderström <kristofer.soderstrom@gmail.com>
2023-02-13 15:31:09 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ descr = '''
 Dissagregates author addresses from Web of Science data.
 Calculates author to address multiplier.
 '''
-
-setuptools.setup(
-	name='geo_address', #
-	version="0.1",
-	author= 'Kristofer R. Söderström',
-	description='Companion package for article "ARTICLE_NAME" published \
-	in "JOURNAL NAME", LICENSE_TYPE, CITATION', #ADD MORE INFO HERE,
-	install_requires=['re', 'pandas', 'numpy', 'tqdm', 'geopy', 'pickle'],
-	packages=["processing.py"]
+if __name__ == "__main__":
+	setuptools.setup(
+		name='geo_address', 
+		version="0.1",
+		author= 'Kristofer R. Söderström',
+		description='Companion package for article "ARTICLE_NAME" published \
+		in "JOURNAL NAME", LICENSE_TYPE, CITATION', #ADD MORE INFO HERE,
+		install_requires=['re', 'pandas', 'numpy', 'tqdm', 'geopy', 'pickle'],
+		packages=["processing.py"]
  	)
