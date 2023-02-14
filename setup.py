1823b576c87b26a13b029a5961bac0e5ad484439
Kristofer Söderström <kristofer.soderstrom@gmail.com>
2023-02-14 09:29:44 +0100

    config

--- setup.py
+++ setup.py
@@ -1,11 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-	name='geo-address', 
-	version="0.1",
-	author= 'Kristofer R. Söderström',
-	description='Companion package for article "ARTICLE_NAME" published \
-	in "JOURNAL NAME", LICENSE_TYPE, CITATION', #ADD MORE INFO HERE,
-	install_requires=['pandas', 'numpy', 'tqdm', 'geopy'],
-	packages=find_packages(exclude=['test'])
- 	)
+if __name__=='__main__':
+	setup()
